62b798c4e502af76178a5e6c3d77714104c16c75
FanteyOfficial <lorida1602@gmail.com>
2023-12-21 17:43:01 +0100

    update

--- inputDinamico.py
+++ inputDinamico.py
@@ -1,27 +1,27 @@
 import pulp
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Define the LP variables and problem as you did before
 x = pulp.LpVariable("x", lowBound=0)
 y = pulp.LpVariable("y", lowBound=0)
 problem = pulp.LpProblem("Un semplice problema di max", pulp.LpMaximize)
 
-obj_func = [20, 12.5]
+excel_file_path = 'model.xlsx'
 
-constraints = [
-    [0.72, 0.23, 13],
-    [2, 0.2, 31],
-    [12, 3, 230],
-    [0.33, 0.28, 10]
-]
+df = pd.read_excel(excel_file_path)
+
+obj_func = df.columns.tolist()
+obj_func.pop()
+
+constraints = df.values.tolist()
 
 problem += obj_func[0] * x + obj_func[1] * y, "The objective function"
 
 for j in range(len(constraints)):
     constraint_name = f"Constraint_{j+1}"
     problem += constraints[j][0] * x + constraints[j][1] * y <= constraints[j][2], constraint_name
-
 
 # Solve the problem
 problem.solve()
@@ -41,10 +41,12 @@ for j in y_values:
     plt.plot(x_values, j, label=f"n")
 
 # Highlight the feasible region
-plt.fill_between(x_values, np.minimum.reduce([x for x in y_values]), color='gray', alpha=0.5, label='Feasible Region')
+plt.fill_between(x_values, np.minimum.reduce([x for x in y_values]),
+                 color='gray', alpha=0.5, label='Feasible Region')
 
 # Highlight the optimal solution
-plt.scatter([pulp.value(x)], [pulp.value(y)], color='red', label='Optimal Solution')
+plt.scatter([pulp.value(x)], [pulp.value(y)], color='red',
+            label='Optimal Solution')
 
 plt.xlabel("x")
 plt.ylabel("y")
