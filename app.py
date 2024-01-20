6bfbf59fbd4d2c46c758a0e7d15144b7998a98e2
FanteyOfficial <lorida1602@gmail.com>
2024-01-20 08:04:43 +0100

    update

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-table_data = [
-    {'Name': 'John', 'Age': 25, 'Country': 'USA'},
-    {'Name': 'Alice', 'Age': 30, 'Country': 'Canada'},
-    {'Name': 'Bob', 'Age': 22, 'Country': 'UK'},
-]
-
-@app.route('/')
-def index():
+"""def index():
     return render_template('index.html', table_data=table_data)
+"""
+
+
+@app.route('/solve_problem', methods=["POST"])
+def solve_problem():
+    data = request.json
+    print(jsonify(data))
+
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
